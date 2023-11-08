0ce77b36e913942dbb4c34dde47b849ac851642b
Rydge Rivard <96926831+rydge-rivard@users.noreply.github.com>
2023-11-08 15:39:57 +0100

    Toggle menu display on click.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("test");
+const dropMenu = (function () {
+  const menuHead = document.querySelector("h3");
+  const menu = document.querySelector(".menu");
+
+  const menuHeadTwo = document.querySelector("h4");
+  const menuTwo = document.querySelector(".menu-two");
+
+  function toggleMenu(element) {
+    element.style.display === "none"
+      ? (element.style.display = "")
+      : (element.style.display = "none");
+  }
+
+  menuHead.addEventListener("click", () => toggleMenu(menu));
+  menuHeadTwo.addEventListener("click", () => toggleMenu(menuTwo));
+})();
